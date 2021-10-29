0c4a48b654976c48b83674ae50add11db34b52e1
Jaira Velasco <jairavelasco2001@gmail.com>
2021-10-29 11:35:55 -0600

    Add find_a_string.py.

---
+++ find_a_string.py
@@ -0,0 +1,9 @@
+# Find a string in Python - HackerRank Solution
+def count_substring(string, sub_string):
+    # Find a string in Python - Hacker Rank Solution START
+    count = 0
+    for i in range(len(string)-len(sub_string)+1):
+        if (string[i:i+len(sub_string)] == sub_string):
+            count += 1
+    return count
+   
